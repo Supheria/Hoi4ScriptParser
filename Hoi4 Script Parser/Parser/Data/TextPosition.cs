c64b732ed30308a08e8cef6e4528147903c6e020
Supheria <Supheria@gmail.com>
2023-07-01 09:01:12 +0800

    extract TextPosition to a readonly struct

---
+++ Hoi4 Script Parser/Parser/Data/TextPosition.cs
@@ -0,0 +1,34 @@
+﻿namespace Parser.Data;
+
+public readonly struct TextPosition : IComparable
+{
+    public uint Line { get; }
+
+    public uint Column { get; }
+
+    public TextPosition(uint line, uint column)
+    {
+        Line = line;
+        Column = column;
+    }
+
+    public TextPosition() : this(0, 0)
+    {
+    }
+
+    public override string ToString()
+    {
+        return $"({Line},{Column})";
+    }
+
+    public int CompareTo(object? obj)
+    {
+        if (obj is not TextPosition other)
+            return -1;
+        var left = (Line, Column);
+        var right = (other.Line, other.Column);
+        return left.CompareTo(right);
+    }
+
+    public override int GetHashCode() => (Line, Column).GetHashCode();
+}
--- Hoi4 Script Parser/Parser/Data/Word.cs
+++ Hoi4 Script Parser/Parser/Data/Word.cs
@@ -4,22 +4,23 @@ public class Word
 {
     public string Text { get; }
 
-    public uint Line { get; }
+    public TextPosition Position { get; }
 
-    public uint Column { get; }
+    public uint Line => Position.Line;
+
+    public uint Column => Position.Column;
 
     public Word(string text, uint line, uint column)
     {
         Text = text;
-        Line = line;
-        Column = column - (uint)text.Length;
+        Position = new(line, column - (uint)text.Length);
     }
 
     public Word() : this("", 0, 0)
     {
     }
 
-    public override string ToString() => $"{Text}({Line},{Column})";
+    public override string ToString() => $"{Text}{Position}";
 
     public static bool operator ==(Word left, Word right) =>
         left.Text == right.Text && left.Line == right.Line && left.Column == right.Column;
@@ -28,13 +29,10 @@ public class Word
 
     public override bool Equals(object? obj)
     {
-        if (obj is not Word word) 
+        if (obj is not Word word)
             return false;
         return this == word;
     }
 
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(Text, Line, Column);
-    }
+    public override int GetHashCode() => (Text, Position).GetHashCode();
 }
